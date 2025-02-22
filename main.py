b2d7e629e730c7e131140b8993c000407132cef7
Craig Bucher <97258442+craigbucher@users.noreply.github.com>
2025-02-21 19:26:41 -0500

    End of lesson

---
+++ main.py
@@ -0,0 +1,27 @@
+from stats import get_num_words, get_num_chars, list_of_dicts
+import sys
+
+def get_book_text(file):
+	with open(file) as f:
+		file_contents = f.read()
+	return(file_contents)
+
+def main():
+	if len(sys.argv) == 1:
+		print('Usage: python3 main.py <path_to_book>')
+		sys.exit(1)
+	else:
+		text = get_book_text(sys.argv[1])
+	words = text.split()
+	num_words = get_num_words(words)
+	print(f'Found {num_words} total words')
+	# chars = get_num_chars(text)
+	# for key, value in chars.items():
+	# 	print(f"'{key}': {value}")
+	list = list_of_dicts(text)
+	for item in list:
+		# I COULDNT FIGURE THIS OUT, EITHER!! 🤦‍♂️️
+		for char, count in item.items():
+			print(f"{char}: {count}")
+		
+main()
---
+++ stats.py
@@ -0,0 +1,35 @@
+def get_num_words(words):
+	return len(words)
+	
+def get_num_chars(text):
+	chars = {}
+	text = text.lower()
+	for char in text:
+		if char not in chars:
+			chars[char] = 1
+		else:
+			chars[char] += 1
+	return chars
+
+def sort_on(dict):
+    # Get the first (and only) value from the dictionary
+    # I COULD NOT FIGURE THIS OUT ON MY OWN !!!!
+    return list(dict.values())[0]
+
+def list_of_dicts(text):
+	chars = {}
+	text = text.lower()
+	for char in text:
+		if char not in chars:
+			chars[char] = 1
+		else:
+			chars[char] += 1
+	list=[]
+	for key, value in chars.items():
+		if key.isalpha():
+			#item = {key:value}
+			list.append({key:value})
+			#list.sort()
+	list.sort(reverse=True, key=sort_on)
+	#print(list)	
+	return list
